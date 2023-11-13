0f8a1c921cca9d203fd45d4d6273b2f7a6a7524f
DrMorrisonTierp <drmorrisontierp@gmail.com>
2023-11-13 01:39:51 +0100

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   tango/rango/views.py 	modified:   tango/tango/__pycache__/settings.cpython-39.pyc 	modified:   tango/tango/__pycache__/urls.cpython-39.pyc 	modified:   tango/tango/settings.py 	modified:   tango/tango/urls.py  Untracked files: 	tango/rango/urls.py 	tango/static/ 	tango/templates/

--- tango/tango/settings.py
+++ tango/tango/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_PATH = BASE_DIR/'templates'
+STATIC_DIR = BASE_DIR/'static'
+MEDIA_DIR = BASE_DIR/'media'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'tango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_PATH],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,12 +66,15 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
 ]
 
 WSGI_APPLICATION = 'tango.wsgi.application'
+
+STATICFILES_DIRS = [STATIC_DIR,]
 
 
 # Database
@@ -115,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- tango/tango/urls.py
+++ tango/tango/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib import settings
 from django.urls import path
+from django.urls import include
+from django.conf.urls.static import static
+from rango import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
